4815612309a1986e2a94f32c9883d2367de8478f
Matahari Kridha <matakridha@users.noreply.github.com>
2022-09-25 20:55:23 +0700

    menambah fungsi love.load

--- main.lua
+++ main.lua
@@ -3,7 +3,13 @@ WINDOW_HEIGHT = 720
 
 
 function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
     
+    -- seed the RNG
+    math.randomseed(os.time())
+
+    -- app window title
+    love.window.setTitle('TugasGit')
 end
 
 function love.update()
